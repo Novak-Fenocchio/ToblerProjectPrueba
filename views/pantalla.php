3939de97c6181384b9f4998a6c6ab5cd176e5f24
jonasnapoles <jno@outlook.pt>
2021-04-01 16:50:18 +0100

    anexo o pagina error404

--- views/pantalla.php
+++ views/pantalla.php
@@ -46,21 +46,71 @@
     <div class="container-fluid bg-light">
         <div class="container">
             <ul class="nav nav-justified py-2 nav-pills">
-            
+                <!-- Ativar botões com variaveis get -->
+                <?php if (isset($_GET["pagina"])) : ?>
 
-                <!--GET: $_GET["VARIAVEL"]-->
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?pagina=registo">Regitos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?pagina=ingreso">Ingresos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?pagina=salir">Salir</a>
-                </li>
+                    <?php if ($_GET["pagina"] == "registo") : ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php?pagina=registo">Regitos</a>
+                        </li>
+
+                    <?php else : ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?pagina=registo">Regitos</a>
+                        </li>
+
+                    <?php endif ?>
+
+                    <?php if ($_GET["pagina"] == "ingreso") : ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php?pagina=ingreso">Ingreso</a>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+                        </li>
+                    <?php endif ?>
+
+                    <?php if ($_GET["pagina"] == "inicio") : ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+                        </li>
+                    <?php endif ?>
+                
+                    <?php if ($_GET["pagina"] == "salir") : ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php?pagina=salir">Sair</a>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?pagina=salir">Sair</a>
+                        </li>
+                    <?php endif ?>
+
+                <?php else : ?>
+
+                    <!--GET: $_GET["VARIAVEL"]-->
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php?pagina=registo">Regitos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?pagina=ingreso">Ingresos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?pagina=salir">Salir</a>
+                    </li>
+
+                <?php endif ?>
+                
             </ul>
         </div>
     </div>
